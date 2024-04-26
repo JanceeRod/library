66ae8a6caf76a0e799b92d848c3aab03e02766ad
Jancee <jancee800@gmail.com>
2024-04-26 12:15:27 +0800

    toggle red and green color for read buttons

--- library.js
+++ library.js
@@ -1,6 +1,22 @@
 const popupModal = document.querySelector('.modal')
 const addBookButton = document.querySelector('.add-book-button')
 
+const readButtons =  document.querySelectorAll('.read')
+
 addBookButton.addEventListener('click', () => {
     popupModal.showModal()
 })
+
+readButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.classList.contains('read')) {
+            button.classList.remove('read');
+            button.classList.add('read-clicked');
+        } else if (button.classList.contains('read-clicked')) {
+            button.classList.remove('read-clicked');
+            button.classList.add('read');
+        } else {
+            button.classList.add('read');
+        }
+    });
+});
