9fc4431d37db4b7b1e974614ebb2b004f43e1777
Jancee <jancee800@gmail.com>
2024-04-26 12:19:45 +0800

    change text when read button is toggled

--- library.js
+++ library.js
@@ -12,9 +12,11 @@ readButtons.forEach(button => {
         if (button.classList.contains('read')) {
             button.classList.remove('read');
             button.classList.add('read-clicked');
+            button.textContent = 'Not Read'
         } else if (button.classList.contains('read-clicked')) {
             button.classList.remove('read-clicked');
             button.classList.add('read');
+            button.textContent = 'Have Read'
         } else {
             button.classList.add('read');
         }
