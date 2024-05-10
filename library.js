c268ed2554f1bf98c505b7e0147ba7c47fc8de0b
Jancee <jancee800@gmail.com>
2024-05-10 16:23:30 +0800

    create library and setup event listeners

--- library.js
+++ library.js
@@ -7,3 +7,24 @@ class Book {
     }
 }
 
+class Library {
+    constructor() {
+        this.books = []
+        this.popupModal = document.querySelector('.modal')
+        this.addBookButton = document.querySelector('.add-book-button')
+        this.modalForm = document.querySelector('.form')
+        this.booksGrid = document.querySelector('.books-grid')
+
+        this.addBookButton.addEventListener('click', () => {
+            this.popupModal.showModal()
+        })
+
+        this.booksGrid.addEventListener('click', (event) => {
+            if (event.target.classList.contains('remove-book')) {
+                this.removeBook(event.target)
+            } else if (event.target.classList.contains('read') || event.target.classList.contains('read-clicked')) {
+                this.toggleReadStatus(event.target)
+            }
+        });
+    }
+}
